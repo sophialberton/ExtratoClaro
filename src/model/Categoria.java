bc942ad546fd0e6ee0374846b86cd0c302561b11
Sophia <sophia.alberton@gmail.com>
2025-04-19 17:23:40 -0300

    test: Criei classe Transacao e Categoria (apenas base, sujeita a ajustes)

--- src/model/Categoria.java
+++ src/model/Categoria.java
@@ -1,3 +1,30 @@
 /*Representa uma categoria criada pelo usuário 
 ou padrão do sistema.
-Campos: nome, palavrasChave, éPersonalizada, etc. */
+Campos: nome, palavrasChave, é Personalizada, etc. */
+
+package model;
+
+class Categoria {
+
+    private String nome;
+    private String[] palavrasChave;
+    private boolean personalizada;
+
+    public Categoria(String nome, String[] palavrasChave, boolean personalizada) {
+        this.nome = nome;
+        this.palavrasChave = palavrasChave;
+        this.personalizada = personalizada;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String[] getPalavrasChave() {
+        return palavrasChave;
+    }
+
+    public boolean isPersonalizada() {
+        return personalizada;
+    }
+}
--- src/model/Transacao.java
+++ src/model/Transacao.java
@@ -1,3 +1,41 @@
 /*Representa uma movimentação bancária.
 Campos: data, descricao, valor, categoria,
  tipo, etc.*/
+
+package model;
+public class Transacao {
+
+    private String data;
+    private String descricao;
+    private double valor;
+    private String categoria;
+    private String tipo; // "entrada" ou "saida"
+
+    public Transacao(String data, String descricao, double valor, String categoria, String tipo) {
+        this.data = data;
+        this.descricao = descricao;
+        this.valor = valor;
+        this.categoria = categoria;
+        this.tipo = tipo;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+}
